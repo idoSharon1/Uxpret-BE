87b21416f6a0034194fc38b79c51370eae2c309e
Yoav David <41908101+yoavdavid2@users.noreply.github.com>
2025-06-14 20:55:16 +0300

    Update reports.controller.ts
    Removed duplicated function

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -40,26 +40,6 @@ export class ReportsController {
     return this.reportsService.getReportHistoryByProjectId(params.projectId);
   }
 
-
-
-  @ApiQuery({
-    name: 'websiteName',
-    required: true,
-    description:
-      'Get all the reposts history of this website that the user did',
-  })
-  async getReportHistoryByName(
-    @Param() params: ReportHistoryParamDto,
-    @Request() req,
-  ) {
-    const userId = req.user?.userId ?? '67e92091e4ef47c4fb3809ab'; // Replace with actual user ID extraction logic
-    return this.reportsService.getReportHistoryByName(
-      params.websiteName,
-      userId,
-    );
-  }
-
-
   @Delete(':id')
   @ApiQuery({ name: 'id', required: true, description: 'Delete report by id' })
   async deleteReport(@Param() params: ReportParamDto, @Request() req) {
